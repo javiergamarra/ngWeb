67cb113f3342eb92d341d2c6eee133f424b53733
Reverendo <rvdo.santiago@gmail.com>
2017-10-13 19:50:55 +0200

    contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { NglModule } from 'ng-lightning/ng-lightning';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
+    NglModule.forRoot(),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  opened: boolean = false;
+  size: string;
+
+  noHeader: boolean = false;
+  noFooter: boolean = false;
+  directional: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+ open(size?: string) {
+    this.size = size;
+    this.opened = !this.opened;
+  }
+
+  cancel() {
+    this.opened = false;
+  }
+}
