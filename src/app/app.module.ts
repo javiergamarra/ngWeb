12c424ed51d9d2f426a5a9dcf099636e57e0eb81
Kiyoshi Omaza <kiyoshi.osaldana@gmail.com>
2017-10-20 23:30:27 +0200

    Componente contact actualizado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { BodyComponent } from './body/body.component';
 import { NglModule } from 'ng-lightning/ng-lightning';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import {FormsModule} from '@angular/forms';
-import {MatMenuModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material';
 import { MenuComponent } from './menu/menu.component';
 import { ContactComponent } from './contact/contact.component';
 
