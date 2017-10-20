e979337fb57536378d46763f3d8c8237d16a18cc
Kiyoshi Omaza <kiyoshi.osaldana@gmail.com>
2017-10-20 19:38:16 +0200

    Actualizando el componente modal

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,19 +12,4 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  opened: boolean = false;
-  size: string;
-
-  noHeader: boolean = false;
-  noFooter: boolean = false;
-  directional: boolean = false;
-
-  open(size?: string) {
-    this.size = size;
-    this.opened = !this.opened;
-  }
-
-  cancel() {
-    this.opened = false;
-  }
 }
