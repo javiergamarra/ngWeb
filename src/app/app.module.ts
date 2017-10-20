3ef77872708dc2e5a989f40f3d0cf4b0b3d8e1f6
santiagopelaez <rvdo.santiago@gmail.com>
2017-10-20 19:16:39 +0200

    ngl added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
-
+import { NglModule } from 'ng-lightning/ng-lightning';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { BsDropdownModule } from 'ngx-bootstrap';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,11 @@ import { MenuComponent } from './menu/menu.component';
     FooterComponent,
     HeaderComponent,
     MenuComponent,
-    BodyComponent
+    BodyComponent,
+    ContactComponent
   ],
   imports: [
+    NglModule.forRoot(),
     BrowserModule,
     BsDropdownModule.forRoot(),
     FormsModule
